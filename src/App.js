b76a74ffa497c6e65b89060b30597e14d0d4eda6
Shani Almog <killerdrum@gmail.com>
2020-10-04 22:55:19 +0300

    first commit, building my new website

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import About from './components/About'
+import Projects from './components/Projects'
+
+const App = () => {
+    return (
+        <div>
+            <Navbar />
+            <h1>New site to be B]</h1>
+            <About />
+            <Projects />
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <ul>
+                <li><a href='#'>About</a></li>
+                <li><a href='#'>Projects</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
